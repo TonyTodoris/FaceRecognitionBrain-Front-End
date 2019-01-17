cd21728bef566d8e43e4ae2a218eddb52656b302
Antonis <antonistodoris@gmail.com>
2019-01-17 15:10:34 +0100

    Added Rank Component styled it and also used Particles.js package to create some background particles, Finalized also the ImageLinkForm component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,46 @@ import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import Rank from './components/Rank/Rank'
 import './App.css';
+import Particles from 'react-particles-js';
+
+
+const particlesOptions ={
+                particles: {
+                  number:{
+                    value: 80,
+                    density: {
+                      enable: true,
+                      value_area: 400
+                    }
+                  }
+              },
+              "interactivity": {
+                  detect_on: "canvas",
+                  events: {
+                    onhover: {
+                    enable: true,
+                    mode: "repulse"
+                    },
+                    onclick: {
+                      enable: true,
+                      mode: "push"
+                      },
+                    resize: true
+                  },
+              }
+            }
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles className='particles' params={particlesOptions}
+        />
         <Navigation />
         <Logo />
+        <Rank />
         <ImageLinkForm />
         {/*
                 <FaceRecognition />*/}
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Rank = () => {
+	return(
+		<div>
+			<div className='white f3'>
+				{'Antonis, your current rank is ...'}
+			</div>
+			<div className='white f5'>
+				{'10'}
+			</div>		
+		</div>
+		
+	);
+}
+
+export default Rank;
